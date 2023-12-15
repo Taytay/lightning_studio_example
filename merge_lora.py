41dc77aef68eeca22e79c764bc1f64ba64435e84
Taylor Brown <taylor@youneedabudget.com>
2023-12-15 13:17:33 -0600

    Fix merge_lora and allow for lora_path specification

--- merge_lora.py
+++ merge_lora.py
@@ -2,21 +2,26 @@ from pathlib import Path
 import sys
 
 sys.path.append("llm-finetune/lit-gpt")
-sys.path.append("llm-finetune/lit-gpt/script")
+sys.path.append("llm-finetune/lit-gpt/scripts")
 
-import merge_lora as merge
+import scripts.merge_lora as merge
 
-
-def merge_lora(
+def perform_merge(
     model_name: str = "mistralai/Mistral-7B-v0.1",
+    lora_path: str = "out/lora/alpaca/lit_model_lora_finetuned.pth",
+    out_dir: str = "out/lora/checkpoint"
 ):
-    mege.merge_lora(
+    out_dir = Path(out_dir)
+    # Make the out_dir, because the merge_lora script doesn't do that...
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    merge.merge_lora(
         checkpoint_dir = Path("llm-finetune/lit-gpt/checkpoints") / model_name,
+        lora_path = Path(lora_path),
         precision = "bf16-true",
     )
-
 
 if __name__ == "__main__":
     from jsonargparse import CLI
 
-    CLI(merge_lora)
+    CLI(perform_merge)
